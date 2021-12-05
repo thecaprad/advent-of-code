dc3071258327e66e7c9009052152692d3abbaca0
Corey <coreycr@hey.com>
2021-12-05 15:34:09 -0600

    Refactor

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -6,38 +6,54 @@ export function normalizeData(data) {
     return result;
 }
 
-export function solutionA(data) {
-    // [0, 0, 0, 0, 0,] — All zeros the length of a single row.
-    var binaryColumnSums = data[0].map(function() {
+function getColumnSums(lists) {
+    // Given a list of equal-lengthed lists of integers, returns a single
+    // list with sum of each column. [[0, 0, 1], [1, 0, 1]] --> [1, 0, 2].
+    var result = lists[0].map(function() {
         return 0;
     });
-    var rawGamaByte = binaryColumnSums.slice(); // [0, 0, 0, 0, 0,]
-    var rawEpsilonByte = binaryColumnSums.slice(); // [0, 0, 0, 0, 0]
-    for (let i = 0; i < data.length; i++) {
-        // Increment above zeroed-out array by each columns's binary sum.
-        // binaryColumnSumns [0, 0, 0, 0, 0,] --> [7, 5, 8, 7, 5].
-        for (let n = 0; n < data[0].length; n++) {
-            binaryColumnSums[n] += parseInt(data[i][n]);
+    for (let i = 0; i < lists.length; i++) {
+        for (let n = 0; n < lists[0].length; n++) {
+            result[n] += parseInt(lists[i][n]);
         }
     }
-    for (let i = 0; i < rawGamaByte.length; i++) {
-        // If a column's total is greater than half the length of input,
-        // the most common int in that column is 1.
-        // Each column in rawGamaByte is set to the most common int per column.
-        // rawEpislonByte columns are set to the opposite. (0 or 1).
-        // [1, 0, 1, 1, 0].
-        rawGamaByte[i] = binaryColumnSums[i] > data.length / 2 ? 1 : 0;
-        // Opposite of the above. [0, 1, 0, 0, 1].
-        rawEpsilonByte[i] = rawGamaByte[i] == 1 ? 0 : 1;
+    return result;
+}
+
+function calculateMostCommonBinary(list, inputLength, opposite = true) {
+    // Given a list of integers, returns a new list of the same length
+    // where each place is 1 if the given int is more than half the length
+    // of the input length. Otherwise it's 0.
+    // [7, 5, 8, 7, 5] --> [1, 0, 1, 1, 0].
+    var result = list.slice();
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] == inputLength / 2) {
+            result[i] = opposite ? 0 : 1;
+        } else {
+            result[i] = list[i] > inputLength / 2 ? 1 : 0;
+            if (opposite) {
+                result[i] = result[i] == 1 ? 0 : 1;
+            }
+        }
     }
-    var gamaBinaryString = '';
-    var epsilonBinaryString = '';
+    return result;
+}
+
+function convertBinaryArrayToString(list, inputLength) {
     // Convert byte arrays to concatenated string.
     // [1, 0, 1, 0, 0] --> '10100.'
-    for (let i = 0; i < rawGamaByte.length; i++) {
-        gamaBinaryString += rawGamaByte[i].toString();
-        epsilonBinaryString += rawEpsilonByte[i].toString();
+    var result = '';
+    for (let i = 0; i < list.length; i++) {
+        result += list[i].toString();
     }
-    // Convert strings to binary and multiply them.
+    return result;
+}
+
+export function solutionA(data) {
+    var binaryColumnSums = getColumnSums(data);
+    var rawGamaByte = calculateMostCommonBinary(binaryColumnSums, data.length, false);
+    var rawEpsilonByte = calculateMostCommonBinary(binaryColumnSums, data.length, true);
+    var gamaBinaryString = convertBinaryArrayToString(rawGamaByte, data.length);
+    var epsilonBinaryString = convertBinaryArrayToString(rawEpsilonByte, data.length);
     return parseInt(gamaBinaryString, 2) * parseInt(epsilonBinaryString, 2);
 }
